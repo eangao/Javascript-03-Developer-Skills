29463d45d76ff814275e37cbab3c04fbde7e4e9a
elmar <elmarcera@gmail.com>
2023-10-27 11:38:39 +0800

    Installing Node.js and Setting Up a Dev Environment

--- script.js
+++ script.js
@@ -35,4 +35,46 @@ const calcAge = birthYear => 2037 - birthYear;
 // is to just create a New Global Snippets File.
 
 // type cl - to output console.log();
-console.log();
+console.log(calcAge(1991));
+
+//////////////////////////////////////////////////////
+// Installing Node.js and Setting Up a Dev Environment
+//////////////////////////////////////////////////////
+
+// And let's actually start with the easier approach
+// which is to simply install the Live Server,
+// the extension here on VS Code.
+
+// So as I said before, there is a more professional way
+// of running Live Server
+// which just by using something called Node.jS,
+// which is a JavaScript runtime
+// that we can install on our computers.
+// Now, if the extension that we just installed works just fine
+// for you and you don't want to add any more complexity,
+// then you can just skip the rest of this video
+// and move on in the course.
+// But I will actually use the second approach
+// in the rest of the course,
+// which is to first install Node.js,
+// and then use an npm package called Live Server there.
+
+// for some reason. Okay?
+// Now, anyway, after you successfully installed Node.js,
+// it will become available as a program
+// in the so-called terminal.
+// and let's use the VS Code built-in terminal.
+// So VS code has a terminal included and to open it,
+
+// number here.
+// It doesn't matter what number,
+// but as long as you see a number,
+// it means that you successfully installed Node.js
+// So if you have this number here
+// then you can now write npm install live-server -g.
+
+// in cmd
+// npm install live-server -g
+
+// to run the live-server
+// live-server
