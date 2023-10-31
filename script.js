ceb176375a216ad41dd0345acfb17efb7cb36720
elmar <elmarcera@gmail.com>
2023-10-31 11:21:29 +0800

    Using Google, StackOverflow and MDN

--- script.js
+++ script.js
@@ -4,38 +4,38 @@
 ////////////////////////////////////////
 // Setting up Prettier and VS Code
 ////////////////////////////////////////
-const x = 23;
+// const x = 23;
 
-// And again, Prettier is an opinionated code formatter.
-// So opinionated means that they have a strong opinion
-// and then basically, when we use the tool,
-// we have to accept that opinion.
-// And many people don't like that
-// but actually, I believe it's a good thing.
-// It makes it so I don't have to take a decision
-// on how the code should look like.
+// // And again, Prettier is an opinionated code formatter.
+// // So opinionated means that they have a strong opinion
+// // and then basically, when we use the tool,
+// // we have to accept that opinion.
+// // And many people don't like that
+// // but actually, I believe it's a good thing.
+// // It makes it so I don't have to take a decision
+// // on how the code should look like.
 
-if (x === 23) console.log(23);
+// if (x === 23) console.log(23);
 
-//   "arrowParens": "always"   -  if you want the single argument birthyear enclose with parenthesis
-const calcAge = birthYear => 2037 - birthYear;
+// //   "arrowParens": "always"   -  if you want the single argument birthyear enclose with parenthesis
+// const calcAge = birthYear => 2037 - birthYear;
 
-// So whenever we want to log something,
-// we have to write all of this console.log
-// and do all this work
-// but in VS Code, we can configure snippets,
-// which can take that work away from us.
-// So let me show you how.
-// So we go here in the menu
-// into this Preferences submenu
-// and then here we can define User Snippets.
-// So click on that.
-// And then you can open one of these files
-// but the best thing to do
-// is to just create a New Global Snippets File.
+// // So whenever we want to log something,
+// // we have to write all of this console.log
+// // and do all this work
+// // but in VS Code, we can configure snippets,
+// // which can take that work away from us.
+// // So let me show you how.
+// // So we go here in the menu
+// // into this Preferences submenu
+// // and then here we can define User Snippets.
+// // So click on that.
+// // And then you can open one of these files
+// // but the best thing to do
+// // is to just create a New Global Snippets File.
 
-// type cl - to output console.log();
-console.log(calcAge(1991));
+// // type cl - to output console.log();
+// console.log(calcAge(1991));
 
 //////////////////////////////////////////////////////
 // Installing Node.js and Setting Up a Dev Environment
@@ -78,3 +78,102 @@ console.log(calcAge(1991));
 
 // to run the live-server
 // live-server
+
+///////////////////////////////////////////
+// Learning How to Code
+///////////////////////////////////////////
+
+/////////////////////////////////////////////////////////
+// How to Think Like a Developer: Become a Problem Solver!
+/////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////
+// Using Google, StackOverflow and MDN
+/////////////////////////////////////////////////////////
+// PROBLEM:
+// we work for a company building a smart homethermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes that sometimes there might be a sensor error."
+
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+// 1) Understanding the problem
+// - What is temp amplitude? Answer: difference beteen highest and lowest temp
+// - Hos to compute max and min temperatures?
+// - What's a sensor error? and what to do?
+
+// 2) Breaking up into sub-problems
+// - How to ignore errors?
+// - Find max value in temp array
+// - Find min value in temp array
+// - Subtract min from max (amplitude) and return it
+
+// ask in google
+// javascript get max value in array
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+// calcTempAmplitude([100, 3, 5, 8, 11]);
+const amplitude = calcTempAmplitude(temperatures);
+console.log(amplitude);
+
+// But now let's suppose that after we're done
+// with our solution, the project manager
+// tells us that the function
+// should actually receive two arrays of temperatures
+// and not just one.
+// But the rest of the function should work just the same.
+// Okay, so well, let's quickly write that problem down here.
+// So problem two,
+// function should now receive two arrays of temperatures.
+// So let's try to again, understand the problem.
+// So we can probably ask the question when we have two arrays,
+// do we need to implement the same functionality twice?
+
+// PROBLEM 2:
+//Function should now recieve 2 arrays of temps
+
+// 1) Understanding the problem
+// - with 2 arrays, should we implement functionality twice? No! Just merge two arrays
+
+// 2) Breaking up into sub-problems
+// - Merge 2 arrays?
+
+// search in google
+//javascrip Merge 2 arrays
+// mdn
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat
+
+const calcTempAmplitudeNew = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+// calcTempAmplitude([100, 3, 5, 8, 11]);
+const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
+console.log(amplitudeNew);
